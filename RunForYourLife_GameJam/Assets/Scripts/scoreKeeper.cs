ba3c5f63a966fdde7ae50646e92969ccbcf32c77
CampbellCKJ <43637015+CampbellCKJ@users.noreply.github.com>
2019-06-27 13:44:57 +1000

    Coin Logic Reimplimented +
    - UI foundation pt1 done
    - Textmesh Pro bits added

---
+++ RunForYourLife_GameJam/Assets/Scripts/scoreKeeper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+public class scoreKeeper : MonoBehaviour
+{
+    public int coins;
+    public int score;
+    public Stopwatch timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void addCoins()
+    {
+        coins++;
+    }
+
+    public void startTimer()
+    {
+        timer = new Stopwatch();
+        timer.Start();
+    }
+}
