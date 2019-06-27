ce5d56fd35c581fa20ccbe9f8be7c2a0f5cfc62f
CampbellCKJ <43637015+CampbellCKJ@users.noreply.github.com>
2019-06-27 15:52:42 +1000

    UI / Sound / CurrencyPickup
    Finished most UI Logic
    Added Temp Sounds in ImportedAssets
    CurrencyPickup logic done

--- RunForYourLife_GameJam/Assets/Scripts/currencyPickup.cs
+++ RunForYourLife_GameJam/Assets/Scripts/currencyPickup.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class currencyPickup : MonoBehaviour
 {
     public scoreKeeper ScoreKeeper;
+    public GameObject prefabSFX;
+    public GameObject Text;
     private void Update()
     {
         this.gameObject.transform.RotateAround(this.gameObject.transform.position, Vector3.up, 360 * Time.deltaTime);
@@ -13,8 +15,10 @@ public class currencyPickup : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            other.GetComponent<AudioSource>().Play(0);
             ScoreKeeper = other.gameObject.GetComponent<scoreKeeper>();
             ScoreKeeper.addCoins();
+            Instantiate(prefabSFX, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
             Destroy(this.gameObject);
         }
     }
--- RunForYourLife_GameJam/Assets/Scripts/scoreKeeper.cs
+++ RunForYourLife_GameJam/Assets/Scripts/scoreKeeper.cs
@@ -9,27 +9,48 @@ public class scoreKeeper : MonoBehaviour
     public int score;
     public Stopwatch timer;
     public Text timerText;
+    private List<int> bestCoins;
+    private List<Stopwatch> bestTime;
+    public GameObject coinText;
+    //public TMPro.Textr
+
     // Start is called before the first frame update
     void Start()
     {
         timer = new Stopwatch();
 
         timer.Start();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         timerText.text = "" + timer.Elapsed;
+        coinText.GetComponent<TMPro.TextMeshPro>().text = "" + coins;
     }
 
     public void addCoins()
     {
         coins++;
+
     }
 
     public void startTimer()
     {
         timer.Start();
+    }
+
+    void runEnded()
+    {
+        timer.Stop();
+        bestTime.Add(timer);
+        
+        timer.Reset();
+        bestCoins.Add(coins);
+        Mathf.Max(bestCoins.ToArray());
+        
+        //add coins to total coins here TO BE DONE WHEN TOTAL COINS ADDED
+        coins = 0;
     }
 }
