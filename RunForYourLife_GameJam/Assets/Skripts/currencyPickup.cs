10c99d82e4b2437dbb8fbda59703fdec86e60e1e
CampbellCKJ <43637015+CampbellCKJ@users.noreply.github.com>
2019-06-27 11:43:36 +1000

    Coin with Interaction
    Added a temporary coin that can be triggered and adds to a coin count.
    Requires use of ScoreKeeper script attached to player to work.

---
+++ RunForYourLife_GameJam/Assets/Skripts/currencyPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class currencyPickup : MonoBehaviour
+{
+    public scoreKeeper ScoreKeeper;
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        this.gameObject.transform.RotateAround(this.gameObject.transform.position, Vector3.up, 360 * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ScoreKeeper = other.gameObject.GetComponent<scoreKeeper>();
+            ScoreKeeper.addCoins();
+            Destroy(this.gameObject);
+        }
+    }
+}
