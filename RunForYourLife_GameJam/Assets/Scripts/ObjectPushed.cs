e0c4bd489fa7a7011d0bce53118a4074231dbad4
Agentsealth <thomas1234lint@gmail.com>
2019-06-28 14:23:06 +1000

    Recovery Project
    Recovery Project

---
+++ RunForYourLife_GameJam/Assets/Scripts/ObjectPushed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPushed : MonoBehaviour
+{
+    public float pushPower = 2.0f;
+
+    public Rigidbody body;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacles")
+        {
+            body = other.gameObject.GetComponent<Rigidbody>();
+            body.constraints = RigidbodyConstraints.None;
+            Vector3 points = new Vector3(Random.Range(1, 3), Random.Range(1, 3), Random.Range(1, 3));
+            Vector3 dir = points - transform.position;
+
+            dir = -dir.normalized;
+
+            body.AddForce(dir * pushPower);
+
+
+        }
+    }
+
+}
--- RunForYourLife_GameJam/Assets/Scripts/menuButtons.cs
+++ RunForYourLife_GameJam/Assets/Scripts/menuButtons.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 
 public class menuButtons : MonoBehaviour
@@ -9,7 +9,9 @@ public class menuButtons : MonoBehaviour
     public GameObject mainMenu;
     public GameObject ingameMenu;
     public GameObject statsMenu;
+    public GameObject gameOverMenu;
     public scoreKeeper ScoreKeeper;
+    
     public Player player;
     public GameObject enemy;
     // Start is called before the first frame update
@@ -45,5 +47,22 @@ public class menuButtons : MonoBehaviour
         mainMenu.SetActive(true);
         statsMenu.SetActive(false);
         ingameMenu.SetActive(false);
+    }
+
+    public void gameOver()
+    {
+        mainMenu.SetActive(false);
+        player.controlsEnabled = false;
+        ingameMenu.SetActive(false);
+        gameOverMenu.SetActive(true);
+        ScoreKeeper.runEnded();
+        mainMenu.SetActive(false);
+        mainMenu.SetActive(false);
+
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene("Programmer_Test_Scene");
     }
 }
